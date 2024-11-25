0864d8e3131385a1d6439235eff56744906a3df2
vitalii.b <vitalii.b@halykmarket.com>
2024-11-25 10:53:50 +0500

    create gateway server

---
+++ planner-gateway/src/main/java/uz/kiverak/micro/planner/plannergateway/PlannerGatewayApplication.java
@@ -0,0 +1,15 @@
+package uz.kiverak.micro.planner.plannergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PlannerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerGatewayApplication.class, args);
+    }
+
+}
