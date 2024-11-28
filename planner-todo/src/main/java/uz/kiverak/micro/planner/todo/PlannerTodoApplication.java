c0593ac8f9eaf87c6e41b543b89c2592b4d2e82a
vitalii.b <vitalii.b@halykmarket.com>
2024-11-28 11:46:49 +0500

    fix aspect

--- planner-todo/src/main/java/uz/kiverak/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/uz/kiverak/micro/planner/todo/PlannerTodoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"uz.kiverak.micro.planner"})
+@ComponentScan(basePackages = {"uz.kiverak.micro.planner.**"})
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
