b2ade80ef054d1c4199eb6b925c615b357000651
kiverak <kiverak@gmail.com>
2024-12-06 09:56:51 +0500

    add RefreshScope to apps

--- planner-todo/src/main/java/uz/kiverak/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/uz/kiverak/micro/planner/todo/PlannerTodoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan(basePackages = {"uz.kiverak.micro.planner"})
 @ComponentScan(basePackages = {"uz.kiverak.micro.planner.**"})
 @EnableFeignClients
+@RefreshScope
 public class PlannerTodoApplication {
 
     public static void main(String[] args) {
--- planner-users/src/main/java/uz/kiverak/micro/planner/users/PlannerUsersApplication.java
+++ planner-users/src/main/java/uz/kiverak/micro/planner/users/PlannerUsersApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"uz.kiverak.micro.planner"})
 @ComponentScan(basePackages = {"uz.kiverak.micro.planner.**"})
+@RefreshScope
 public class PlannerUsersApplication {
 
     public static void main(String[] args) {
